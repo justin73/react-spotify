75f9099f1a144916623eeb4e0960f3d37688416d
Meng Jia <jiameng_73@hotmail.com>
2018-02-09 22:30:47 -0500

    load artist profile image

--- src/component/artistItem.js
+++ src/component/artistItem.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 
 export const ArtistItem = (props) => {
+  let image
+  props.images.length ? image = <img src={props.images[1].url}/> : image = <div>No Image Available</div>
   return (
-    <li>{props.name}</li>
+    <li>
+      <div>
+        {image}
+        <p>{props.name}</p>
+      </div>
+    </li>
   )
 }
